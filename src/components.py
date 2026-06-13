a6364c23b229815b7dad83a0fc0e782099c1c444
bharat5007 <bharatgoyal5007@gmail.com>
2026-06-13 20:53:09 +0530

    RoPE optimized

--- src/components.py
+++ src/components.py
@@ -1,5 +1,4 @@
 import torch
-import math
 import torch.nn as nn
 
 
@@ -27,33 +26,57 @@ class SwiGLU(nn.Module):
         return output * self.layer2(x)
 
 
+############################# This is discarded due to TC (B*T*C**2) #################################
+# class RoPE(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+
+#     def calculate_rope(self, pos, dim):
+#         x = []
+#         for i in range(dim):
+#             tensor = torch.zeros(dim)
+#             if i % 2:
+#                 theta = pos * (10000 ** (-(i - 1) / dim))
+#                 tensor[i - 1] = math.sin(theta)
+#                 tensor[i] = math.cos(theta)
+#             else:
+#                 theta = pos * (10000 ** (-i / dim))
+#                 tensor[i] = math.cos(theta)
+#                 tensor[i + 1] = -math.sin(theta)
+
+#             x.append(tensor)
+
+#         return torch.stack(x)
+
+#     def forward(self, x: torch.Tensor):
+#         logits = []
+#         B, T, C = x.shape
+
+#         for i in range(T):
+#             rope = self.calculate_rope(i, C)
+#             logits.append(x[:, i, :] @ rope)
+
+#         return torch.stack(logits, dim=1)
+
+
 class RoPE(nn.Module):
-    def __init__(self):
+    def __init__(self, T, C):
         super().__init__()
+        freqs = self.calculate_freq
+        self.cos = freqs.cos()
+        self.sin = freqs.sin()
 
-    def calculate_rope(self, pos, dim):
-        x = []
-        for i in range(dim):
-            tensor = torch.zeros(dim)
-            if i % 2:
-                theta = pos * (10000 ** (-(i - 1) / dim))
-                tensor[i - 1] = math.sin(theta)
-                tensor[i] = math.cos(theta)
-            else:
-                theta = pos * (10000 ** (-i / dim))
-                tensor[i] = math.cos(theta)
-                tensor[i + 1] = -math.sin(theta)
-
-            x.append(tensor)
-
-        return torch.stack(x)
+    def calculate_freq(self, T, C):
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, C, 2) / C))
+        positions = torch.arange(T)
+        return torch.outer(positions, inv_freq)
 
     def forward(self, x: torch.Tensor):
-        logits = []
-        B, T, C = x.shape
+        x1 = x[..., ::2]
+        x2 = x[..., 1::2]
 
-        for i in range(T):
-            rope = self.calculate_rope(i, C)
-            logits.append(x[:, i, :] @ rope)
-
-        return torch.stack(logits, dim=1)
+        out_even = x1 * self.cos - x1 * self.sin
+        out_odd = x2 * self.cos + x2 * self.sin
+        out = torch.stack((out_even, out_odd), dim=-1)
+        out = out.flatten(-2)
+        return out
